0c2247f17c454ce724e2ce36a226c666e6e78676
OvoKore <ovokore@hotmail.com>
2023-09-13 22:50:54 -0300

    TRABALHO2

--- app/src/main/java/com/ovokore/acoesnabolsa/AcaoListaActivity.java
+++ app/src/main/java/com/ovokore/acoesnabolsa/AcaoListaActivity.java
@@ -1,23 +1,43 @@
 package com.ovokore.acoesnabolsa;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AcaoListaActivity extends AppCompatActivity {
+import com.ovokore.acoesnabolsa.model.Acao;
+import com.ovokore.acoesnabolsa.persistence.AppDatabase;
+import com.ovokore.acoesnabolsa.utils.Utils;
 
-    private Button btnAcaoCadastro;
+import java.util.List;
+
+    public class AcaoListaActivity extends AppCompatActivity {
+
+//    private Button btnAcaoCadastro;
+
+    private ListView listViewAcoes;
+    private List<Acao> acoes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acao_lista);
 
-        btnAcaoCadastro = findViewById(R.id.btnAcaoCadastro);
+//        btnAcaoCadastro = findViewById(R.id.btnAcaoCadastro);
+//        btnAcaoCadastro.setOnClickListener(v -> startActivity(new Intent(AcaoListaActivity.this, AcaoCadastroActivity.class)));
 
-        btnAcaoCadastro.setOnClickListener(v -> startActivity(new Intent(AcaoListaActivity.this, AcaoCadastroActivity.class)));
+        listViewAcoes = findViewById(R.id.listViewAcoes);
+        listViewAcoes.setOnItemClickListener(
+                (parent, view, position, id) -> {
+                    Acao acao = (Acao) listViewAcoes.getItemAtPosition(position);
+                    Utils.showToast(getApplicationContext(), acao.getCodigo());
+                });
+        popularLista();
+    }
 
+    private void popularLista() {
+        acoes = AppDatabase.getDatabase(getApplicationContext()).acaoDao().getAll();
+        AcaoAdapter acaoAdapter = new AcaoAdapter(this, acoes);
+        listViewAcoes.setAdapter(acaoAdapter);
     }
 }
--- app/src/main/java/com/ovokore/acoesnabolsa/TransacaoListaActivity.java
+++ app/src/main/java/com/ovokore/acoesnabolsa/TransacaoListaActivity.java
@@ -1,23 +1,46 @@
 package com.ovokore.acoesnabolsa;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.ovokore.acoesnabolsa.model.Transacao;
+import com.ovokore.acoesnabolsa.persistence.AppDatabase;
+import com.ovokore.acoesnabolsa.utils.Utils;
+
+import java.util.List;
+
 public class TransacaoListaActivity extends AppCompatActivity {
 
-    private Button btnTransacaoCadastro;
+//    private Button btnTransacaoCadastro;
+
+
+    private ListView listViewAcoes;
+    private List<Transacao> transacoes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transacao_lista);
 
-        btnTransacaoCadastro = findViewById(R.id.btnTransacaoCadastro);
+//        btnTransacaoCadastro = findViewById(R.id.btnTransacaoCadastro);
+//        btnTransacaoCadastro.setOnClickListener(v -> startActivity(new Intent(TransacaoListaActivity.this, TransacaoCadastroActivity.class)));
 
-        btnTransacaoCadastro.setOnClickListener(v -> startActivity(new Intent(TransacaoListaActivity.this, TransacaoCadastroActivity.class)));
+        listViewAcoes = findViewById(R.id.listViewAcoes);
+        listViewAcoes.setOnItemClickListener(
+                (parent, view, position, id) -> {
+                    Transacao transacao = (Transacao) listViewAcoes.getItemAtPosition(position);
+                    Utils.showToast(getApplicationContext(),
+                            transacao.getTipoTransacao() + " " + transacao.getQuantidade()
+                    );
+                });
+        popularLista();
+    }
 
+    private void popularLista() {
+        transacoes = AppDatabase.getDatabase(getApplicationContext()).transacaoDao().getAll();
+        TransacaoAdapter transacaoAdapter = new TransacaoAdapter(this, transacoes);
+        listViewAcoes.setAdapter(transacaoAdapter);
     }
 }
